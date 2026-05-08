de47d0279d81b49b66e66a0d97e43fea5aec04ea
greenhornet63603 <greenhornet63603@gmail.com>
2026-05-09 02:34:27 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,37 +1,133 @@
-class DummyDB:
-    async def add_user(self, *args, **kwargs):
-        pass
+import asyncio
+import logging
+import os
+import shutil
 
-    async def is_user_exist(self, *args, **kwargs):
-        return False
+from pyrogram import Client, raw, types
+from typing import Iterable, List, Union
 
-    async def get_all_users(self, *args, **kwargs):
-        return []
+from bot.config import Config
+from bot.utils import add_admin, start_webserver, set_commands
 
-    async def total_users_count(self, *args, **kwargs):
-        return 0
+import pyromod
 
-    async def delete_user(self, *args, **kwargs):
-        pass
-
-    async def set_config(self, *args, **kwargs):
-        pass
-
-    async def get_config(self, *args, **kwargs):
-        return None
-
-    async def save_file(self, *args, **kwargs):
-        pass
-
-    async def get_file(self, *args, **kwargs):
-        return None
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("apscheduler").setLevel(logging.WARNING)
 
 
-class Database:
+class Bot(Client):
+
     def __init__(self):
-        self.users = DummyDB()
-        self.config = DummyDB()
-        self.files = DummyDB()
+        super().__init__(
+            "bot",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            plugins=dict(root="bot/plugins"),
+        )
 
+    async def start(self):
+        await super().start()
 
-db = Database()
+        os.makedirs("downloads", exist_ok=True)
+        os.makedirs("Videos", exist_ok=True)
+
+        me = await self.get_me()
+
+        self.owner = await self.get_users(
+            int(Config.OWNER_ID)
+        )
+
+        self.username = f"@{me.username}"
+
+        logging.info("Bot started")
+        logging.info(f"Owner: {self.owner.mention}")
+
+        await add_admin(self.owner.id)
+
+        await set_commands(self)
+
+        if Config.WEB_SERVER:
+            await start_webserver()
+
+    async def stop(self, *args):
+        shutil.rmtree("downloads", ignore_errors=True)
+        shutil.rmtree("Videos", ignore_errors=True)
+
+        await super().stop()
+
+    async def get_users(
+        self,
+        user_ids: Union[int, str, Iterable[Union[int, str]]],
+        raise_error: bool = True,
+        limit: int = 200,
+    ):
+
+        is_iterable = not isinstance(user_ids, (int, str))
+
+        user_ids = (
+            list(user_ids)
+            if is_iterable
+            else [user_ids]
+        )
+
+        users = types.List()
+
+        user_ids_chunks = [
+            user_ids[i:i + limit]
+            for i in range(0, len(user_ids), limit)
+        ]
+
+        async def resolve(user_id):
+            try:
+                return await self.resolve_peer(user_id)
+
+            except Exception:
+
+                if raise_error:
+                    raise
+
+                return user_id
+
+        for chunk in user_ids_chunks:
+
+            chunk_resolved = await asyncio.gather(
+                *[
+                    resolve(i)
+                    for i in chunk
+                    if i is not None
+                ]
+            )
+
+            blocked_accounts = [
+                i for i in chunk_resolved
+                if isinstance(i, int)
+            ]
+
+            chunk_resolved = list(
+                filter(None, chunk_resolved)
+            )
+
+            chunk_resolved = [
+                i for i in chunk_resolved
+                if not isinstance(i, int)
+            ]
+
+            if chunk_resolved:
+
+                r = await self.invoke(
+                    raw.functions.users.GetUsers(
+                        id=chunk_resolved
+                    )
+                )
+
+                for i in r:
+                    users.append(
+                        types.User._parse(self, i)
+                    )
+
+            for i in blocked_accounts:
+                users.append(i)
+
+        return users if is_iterable else users[0]
