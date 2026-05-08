d9fb1cd91d1d7d2671b5e26162f0411c822fb998
greenhornet63603 <greenhornet63603@gmail.com>
2026-05-09 02:25:29 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,12 +1,37 @@
-from .users import UserDatabase
-from bot.config import Config
-from .config import ConfigDB
-from .files import FilesDatabase
+class DummyDB:
+    async def add_user(self, *args, **kwargs):
+        pass
+
+    async def is_user_exist(self, *args, **kwargs):
+        return False
+
+    async def get_all_users(self, *args, **kwargs):
+        return []
+
+    async def total_users_count(self, *args, **kwargs):
+        return 0
+
+    async def delete_user(self, *args, **kwargs):
+        pass
+
+    async def set_config(self, *args, **kwargs):
+        pass
+
+    async def get_config(self, *args, **kwargs):
+        return None
+
+    async def save_file(self, *args, **kwargs):
+        pass
+
+    async def get_file(self, *args, **kwargs):
+        return None
+
 
 class Database:
     def __init__(self):
-        self.users = UserDatabase(Config.DATABASE_URL, Config.DATABASE_NAME)
-        self.config = ConfigDB(Config.DATABASE_URL, Config.DATABASE_NAME)
-        self.files = FilesDatabase(Config.DATABASE_URL, Config.DATABASE_NAME)
+        self.users = DummyDB()
+        self.config = DummyDB()
+        self.files = DummyDB()
+
 
 db = Database()
